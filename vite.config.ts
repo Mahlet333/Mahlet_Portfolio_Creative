4f59f89bf98b2d95e6d69ed3067b3e92171264aa
Mahlet333 <132096863+Mahlet333@users.noreply.github.com>
2025-06-22 14:40:05 +0400

    fix: Update vite config base path for correct GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Mahlet_Creative_Portfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Mahlet_Portfolio_Creative/' : '/',
   server: {
     port: 5173,
     open: true
